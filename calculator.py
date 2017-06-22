8ed12f0e808adeec85ab6c5e9780b5a75cf80819
lixianmao <lixianmao@1b2b.cn>
2017-06-22 17:30:38 +0800

    '优化函数支持，美化界面，新增帮助菜单'

--- calculator.py
+++ calculator.py
@@ -11,7 +11,8 @@
 import os
 import sys
 import math
-from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QPushButton, QLineEdit
+from math import *
+from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox, QPushButton, QMenu, QDialog, QTableWidget, QTableWidgetItem, QHBoxLayout
 from PyQt5 import QtCore
 from PyQt5 import uic
 
@@ -27,6 +28,14 @@ class Calculator(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.setWindowTitle(u'程序员计算器')
         self._addButtons()
+        # lineEdit回车事件响应
+        self.lineEdit.returnPressed.connect(self._lineEdit_enterPressed)
+        self._lineEdit_enter_mark = False  # 回车标志
+
+        # 菜单栏-about点击事件响应
+        self.actionabout.triggered.connect(self._about)
+        # 菜单栏-help点击事件响应
+        self.actionhelp.triggered.connect(self._help)
 
     def _addButtons(self):
         '''
@@ -50,14 +59,31 @@ class Calculator(QMainWindow, Ui_MainWindow):
             '(': u'左括号',
             ')': u'右括号',
             'DEL': u'删除',
-            '=': u'结果'
+            '=': u'结果',
         }
 
         # 所有的符号集合
         symbols = tuple(symbol_hex) + tuple(symbol_dict.keys())
 
+        # 按键颜色样式
+        qss_dict = {
+            'normal': '''QPushButton{background-color:#00688B;border:none;color:#eff0f1;font-size:24px;} 
+                        QPushButton:hover{background-color:#333333;}''',
+            'red': '''QPushButton{background-color:#D35400;border:none;color:#ffffff;font-size:24px;} 
+                        QPushButton:hover{background-color:#333333;}''',
+            'green': '''QPushButton{background-color:#16A085;border:none;color:#ffffff;font-size:24px;} 
+                        QPushButton:hover{background-color:#333333;}''',
+        }
+
         for i, name in enumerate(symbols):
             button = QPushButton(name)
+            if name == 'DEL':
+                button.setStyleSheet(qss_dict['red'])
+            elif name == '=':
+                button.setStyleSheet(qss_dict['green'])
+            else:
+                button.setStyleSheet(qss_dict['normal'])
+
             button.clicked.connect(self._onButton)
             if name in symbol_hex:
                 # 十进制/十六进制数值
@@ -67,46 +93,48 @@ class Calculator(QMainWindow, Ui_MainWindow):
             # if name >='A' and name <='F':
             #     button.setEnabled(False)
 
-            COL_NUM = 6  # 每行的列数
+            COL_NUM = 5  # 每行的列数
             row = i / COL_NUM  # 行
             col = i % COL_NUM  # 列
             self.gridLayout.addWidget(button, row, col)
 
     def _onButton(self):
         '''按键动作'''
-        isResult=False #结果标志
+        isResult = False  # 结果标志
 
-        sender = self.sender()
-
-        # 记录该按键的符号
-        symbol = sender.text()
+        if self._lineEdit_enter_mark:
+            self._lineEdit_enter_mark = False  # 清除回车标志
+            symbol = '='
+        else:
+            sender = self.sender()
+            # 记录该按键的符号
+            symbol = sender.text()
         editText = self.lineEdit.text()
         if symbol == 'DEL':
-            #删除
+            # 删除
             editText = editText[:-1]
         elif symbol == '=':
-            isResult=True
-            #更新表达式
+            isResult = True
+            # 更新表达式
 
-            #计算结果
+            # 计算结果
             if editText:
-                result_num,result_str = self.cal(editText)    
-                self.label.setText(u'结果：'+editText+'='+result_str)
+                result_num, result_str = self.cal(editText)
+                self.label.setText(u'结果：' + editText + '=' + result_str)
 
-                result=int(result_num)   
-                label=u'十进制：'+str(result)+u' 二进制: '+bin(result)+u' 十六进制: '+hex(result)+u' 八进制: '+oct(result)
+                result = int(result_num)
+                label = u'十进制：' + str(result) + u' 二进制: ' + bin(result) + \
+                    u' 十六进制: ' + hex(result) + u' 八进制: ' + oct(result)
                 self.lineEdit_convert.setText(label)
                 self.lineEdit_convert.setToolTip(label)
-                
+
         elif symbol in ('x', 'b', 'o'):
             # 进制标志，自动补充0
             editText = editText + '0' + symbol
         else:
             editText = editText + symbol
 
-        self.lineEdit.setText(editText)    
-
-
+        self.lineEdit.setText(editText)
 
     def cal(self, strExp):
         '''计算
@@ -115,25 +143,114 @@ class Calculator(QMainWindow, Ui_MainWindow):
         @return:
             计算的结果，Tuple类型 (number,str)
         '''
-        #指数运算，替换为python识别的模式
-        strExp=strExp.replace('^','**')
-        #函数运算，替换为math.xxx
-                
-        for f in dir(math):
-            if f+'(' in strExp: #f+'('确保f为一个函数
-                strExp=strExp.replace(f,'math.'+f)
-        
-        try:
-            result = eval(strExp.replace('^','**'))
-            ret=(result,str(result))
-        except ZeroDivisionError:
-            QMessageBox.information(self, u'错误', u'除零！')
-            ret=(0,'Error')
-        except:
-            QMessageBox.information(self, u'错误', u'表达式错误！')
-            ret=(0,'Error')
-        return ret 
+        # 删除无效的空格
+        strExp = strExp.replace(' ', '')
+        # 指数运算，替换为python识别的模式
+        strExp = strExp.replace('^', '**')
 
+        try:
+            result = eval(strExp)
+            ret = (result, str(result))
+        except ZeroDivisionError:
+            QMessageBox.critical(self, u'错误', u'除零！')
+            ret = (0, 'Error')
+        except:
+            QMessageBox.critical(self, u'错误', u'表达式错误！')
+            ret = (0, 'Error')
+        return ret
+
+    def _lineEdit_enterPressed(self):
+        '''输入框中回车键按下事件'''
+        self._lineEdit_enter_mark = True  # 标志回车键按下
+        self._onButton()
+
+    def _about(self):
+        '''关于信息单击事件'''
+        QMessageBox.about(
+            self, u'关于', u'<h3><font color="blue">程序员计算器 v 0.5.1 <br> By lxm</font></h3>')
+
+    def _help(self):
+        '''帮助信息点击事件'''
+        h = Help(self)
+        
+        h.show()
+
+        # QMessageBox.about(self,'帮助',text)
+
+
+class Help(QDialog):
+    '''自定义帮助弹出窗'''
+
+    def __init__(self, parent=None):
+        super(Help, self).__init__(parent)
+        self.setWindowTitle(u'帮助信息')
+        # self.setWindowFlags(QtCore.Qt.WindowTitleHint)
+        d = {
+            'acos': u'反余弦',
+            'acosh': u'反双曲余弦',
+            'asin': u'反正弦',
+            'asinh': u'反双曲正弦',
+            'atan': u'反正切',
+            'atan2': u'反正切',
+            'atanh': u'反双曲正切',
+            'ceil': u'≧ 最小整數',
+            'copysign': u'返回与 y 同号的 x 值',
+            'cos': u'余弦',
+            'cosh': u'双曲余弦',
+            'degrees': u'(弧长) 转成角度',
+            'e': u'%s' % e,
+            'erf': u' e^x',
+            'erfc': u'',
+            'exp': u'e^x',
+            'expm1': u'',
+            'fabs': u'',
+            'factorial': u'',
+            'floor': u'≦ x 的最大整数',
+            'fmod': u'x对y取模的余数',
+            # 'frexp': u'',
+            'fsum': u'阵列值的各項和',
+            'gamma': u'',
+            'gcd': u'',
+            'hypot': u'',
+            'inf': u'',
+            'ldexp': u'',
+            'lgamma': u'',
+            'log': u'',
+            'log10': u'',
+            'log1p': u'',
+            'log2': u'',
+            'modf': u'',
+            'nan': u'',
+            'pi': u'%s' % pi,
+            'pow': u'',
+            'radians': u'',
+            'sin': u'',
+            'sinh': u'',
+            'sqrt': u'',
+            'tan': u'',
+            'tanh': u'',
+            'tau': u'%s'%tau,
+            'trunc': u''
+        }
+        self.table = QTableWidget(len(d), 2)  # 行，列数目,行数必须>=实际行数
+        self.table.setHorizontalHeaderLabels([u'名称', u'说明'])
+
+        i = 0
+        for name,value in d.items():
+            # 设置第一列
+            item_name = QTableWidgetItem(name)
+            self.table.setItem(i, 0, item_name)
+            # 设置第二列
+            item_value = QTableWidgetItem(value)
+            self.table.setItem(i, 1, item_value)
+            i += 1
+            
+
+        self.table.setRowCount(i)
+
+        layout = QHBoxLayout()
+        layout.addWidget(self.table)
+        self.setLayout(layout)
 
 
 def main():
